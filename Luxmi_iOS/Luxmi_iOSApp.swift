1f9ec45d907b038458ae7e8452e415a7d7eeb5a2
MrityunjaySaha <awesomedbz@gmail.com>
2023-05-10 19:07:40 -0700

    Initial Commit

---
+++ Luxmi_iOS/Luxmi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Luxmi_iOSApp.swift
+//  Luxmi_iOS
+//
+//  Created by Mrityunjoy Saha on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Luxmi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
